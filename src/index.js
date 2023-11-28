cb1d0dd2df4cefcb4c58c5d7b6ab32b41ff93a41
user01 <user01@user01noMacBook-Air.local>
2023-11-29 02:44:15 +0900

    認証機能修正

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+import config from './aws-exports';
+Amplify.configure(config);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
